064769fab96bb729e5a586bd8c33fda967934355
Luca Pireddu <pireddu@crs4.it>
2025-05-12 12:35:43 +0200

    style: whitespace and other formatting

--- bbmri_fp_etl/destinations/fhir.py
+++ bbmri_fp_etl/destinations/fhir.py
@@ -2,11 +2,18 @@
 #
 # This file is part of BBMRI-FP-ETL.
 #
-# BBMRI-FP-ETL is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# BBMRI-FP-ETL is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# BBMRI-FP-ETL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# BBMRI-FP-ETL is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 #
-# You should have received a copy of the GNU Affero General Public License along with BBMRI-FP-ETL. If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with BBMRI-FP-ETL. If not, see <https://www.gnu.org/licenses/>.
 
 from fhirclient.models.age import Age
 from fhirclient.models.bundle import Bundle, BundleEntry, BundleEntryRequest
